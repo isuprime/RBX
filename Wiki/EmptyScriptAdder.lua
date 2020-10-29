45e76d996a0900374d49b4c66e82ed631f83e369
isuprime <71159832+isuprime@users.noreply.github.com>
2020-10-29 16:59:36 -0600

    Update EmptyScriptAdder.lua

--- Wiki/EmptyScriptAdder.lua
+++ Wiki/EmptyScriptAdder.lua
@@ -1,6 +1,3 @@
-local ChangeStoryService = game:GetService("ChangeHistoryService")
-
-
 local toolbar = plugin:CreateToolbar("Custom Script Tools")
 local newScriptButton = toolbar:CreateButton("Create Empty Script", "Create An Empty Script", "rbxassetid://4458901886")
 
@@ -8,7 +5,7 @@ local function OnNewScriptButtonClicked()
 	local newScript = Instance.new("Script")
 	newScript.Source = ""
 	newScript.Parent = game:GetService("ServerScriptService")
-	ChangeStoryService:SetWaypoint("Added new empty script")
+
 end
 
 newScriptButton.Click:Connect(OnNewScriptButtonClicked)
